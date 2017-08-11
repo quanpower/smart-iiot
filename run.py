1f67f395850781b56c9a57d7df017f2f778648eb
quanpower <quanpower@gmail.com>
2017-08-12 01:50:41 +0800

    reverse the temps by datetime

--- run.py
+++ run.py
@@ -38,7 +38,7 @@ class LoraTemps(Resource):
         for i in xrange(len(temp_records)):
             temp_log.append({"time":temp_records[i][3].strftime("%Y-%m-%d %H:%M:%S"),"Temp1":temp_records[i][0],"Temp2":temp_records[i][1],"Temp3":temp_records[i][2]})
         temps_reverse = temp_log.reverse()
-        print(temps_log)
+        print(temp_log)
         print(temps_reverse)
 
         temps_dict = {"temps":temp_log.reverse()}
