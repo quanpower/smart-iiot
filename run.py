1acaa604e1b0760212be62533bac1e69192da9cb
quanpower <quanpower@gmail.com>
2017-08-22 01:07:11 +0800

    * 1.edit api loranode_temp

--- run.py
+++ run.py
@@ -60,11 +60,11 @@ class LoraTemp(Resource):
     #         """
     def get(self, gatewayAddr, nodeAddr):
 
-        get_parser = reqparse.RequestParser()
-        get_parser.add_argument('id', type=int, location='args', required=True)
-
-        args = get_parser.parse_args()
-        id = args.get('id')
+        # get_parser = reqparse.RequestParser()
+        # get_parser.add_argument('id', type=int, location='args', required=True)
+        #
+        # args = get_parser.parse_args()
+        # id = args.get('id')
 
         temps = db.session.query(GrainTemp.temp1, GrainTemp.temp2, GrainTemp.temp3, GrainTemp.battery_vol).filter(and_(LoraGateway.gateway_addr == gatewayAddr, LoraNode.node_addr == nodeAddr)).order_by(GrainTemp.datetime.desc()).first()
         temp_dic = {"numbers": [{"icon": "apple", "color": "#64ea91", "title": "温度1", "number": temps[0]}, {"icon": "apple", "color": "#8fc9fb", "title": "温度2", "number": temps[1]}, {"icon": "apple", "color": "#d897eb", "title": "温度3", "number": temps[2]}, {"icon": "message", "color": "#f69899", "title": "电池", "number": temps[3]}]}
