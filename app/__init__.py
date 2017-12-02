a357df0ab60537904addfd9da9ed1b4410987825
quanpower <quanpower@gmail.com>
2017-12-02 10:50:15 +0800

    * 1.add dist to static

--- app/__init__.py
+++ app/__init__.py
@@ -83,7 +83,7 @@ def register_blueprints(app):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(api_blueprint, url_prefix='/api/v2')
 
 
 
