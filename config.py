946a8746f6ff12735b54951a5be5ce79d17872d3
quanpower <quanpower@gmail.com>
2017-12-12 20:35:27 +0800

    edit config

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config:
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
     FLASKY_SLOW_DB_QUERY_TIME = 0.5
+    SERVER_NAME = 'www.aiwcy.com'
 
     @staticmethod
     def init_app(app):
