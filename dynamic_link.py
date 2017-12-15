e149f5cc05993d40081c62148d551020ceed7502
quanpower <quanpower@gmail.com>
2017-12-15 13:18:49 +0800

    edit dynamic_link

--- dynamic_link.py
+++ dynamic_link.py
@@ -141,13 +141,14 @@ def dynamic_link():
                     print(power_io_addr)
                     print(type(power_io_addr))
                     print(suck_func_code[0])
+                    test_addr = '4'
 
                     if power_io_addr and suck_func_code[0]:
                         print('----send mqtt to cut off!------')
-                        transmitMQTT_byte(power_io_addr, suck_func_code[0])
+                        transmitMQTT_byte(test_addr, suck_func_code[0])
                         time.sleep(10)
-                        # transmitMQTT_byte(power_io_addr, release_func_code[0])
-                        # time.sleep(10)
+                        transmitMQTT_byte(test_addr, release_func_code[0])
+                        time.sleep(10)
                         print('-------mqtt sended over!-------')
                         print('\n' * 3)
 
