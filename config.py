633ce50ee73ee1977aefce159a240bdd3f8339d9
quanpower <quanpower@gmail.com>
2017-12-18 12:44:55 +0800

    edit config

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[维创云]'
-    FLASKY_MAIL_SENDER = 'WCY Admin <2710313367@qq.com>'
+    FLASKY_MAIL_SENDER = 'WCY Admin <252527676@qq.com>'
     FLASKY_ADMIN = os.environ.get('WCY_ADMIN')
     # SERVER_NAME = os.environ.get('SERVER_NAME')
     SSL_REDIRECT = False
